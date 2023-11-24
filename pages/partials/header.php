1622d0bc112a7fc459376006fb9e39dcbe430452
Yasir Amri <yasiramrr@gmail.com>
2023-11-24 16:23:24 +0700

    update navbar

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -1,81 +1,86 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow" style="padding-top: 20px; padding-bottom: 20px;">
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow" style="padding-top: 5px; padding-bottom: 5px;">
   <div class="container">
+    <!-- Add the navbar brand with your logo -->
+    <a class="navbar-brand" href="#">
+      <img src="./assets/img/Logo-CORALGUARD.png" alt="Logo" style="width: 80px;"> <!-- Adjust width and height as needed -->
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item" style="padding-right: 20px;">
-          <?php if ($_GET["page"] == "home") : ?>
-            <a class="nav-link" style="color: red; font-weight: bold;" aria-current="page" href="?page=home">Home</a>
-          <?php else : ?>
-            <a class="nav-link" aria-current="page" href="?page=home">Home</a>
-          <?php endif ?>
-        </li>
-        <li class="nav-item" style="padding-right: 20px;">
-          <?php if ($_GET["page"] == "about") : ?>
-            <a class="nav-link" style="color: red; font-weight: bold;" href="?page=about">About</a>
-          <?php else : ?>
-            <a class="nav-link" href="?page=about">About</a>
-          <?php endif ?>
-        </li>
-        <li class="nav-item dropdown" style="padding-right: 20px;">
-          <?php if ($_GET["page"] == "rekomendasi" || $_GET["page"] == "jenis-detail" || $_GET['page'] == "jenis" || $_GET['page'] == "detail-page" || $_GET["page"] == "komunitas" || $_GET["page"] == "koleksi-saya") : ?>
-            <a class="nav-link dropdown-toggle" style="color: red; font-weight: bold;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item" style="padding-right: 20px;">
+            <?php if ($_GET["page"] == "home") : ?>
+              <a class="nav-link" style="color: #0089ed; font-weight: bold;" aria-current="page" href="?page=home">Home</a>
             <?php else : ?>
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <?php endif ?>
-              Program
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                <?php if ($_GET["page"] == "detail-page" || $_GET["page"] == "jenis" || $_GET["page"] == "jenis-detail") : ?>
-                  <a class="dropdown-item active" href="?page=jenis">
-                    Biodiversity
-                  </a>
-                <?php else : ?>
-                  <a class="dropdown-item" href="?page=jenis">
-                    Biodiversity
-                  </a>
+              <a class="nav-link" aria-current="page" href="?page=home">Home</a>
+            <?php endif ?>
+          </li>
+          <li class="nav-item" style="padding-right: 20px;">
+            <?php if ($_GET["page"] == "about") : ?>
+              <a class="nav-link" style="color: #0089ed; font-weight: bold;" href="?page=about">About</a>
+            <?php else : ?>
+              <a class="nav-link" href="?page=about">About</a>
+            <?php endif ?>
+          </li>
+          <li class="nav-item dropdown" style="padding-right: 20px;">
+            <?php if ($_GET["page"] == "rekomendasi" || $_GET["page"] == "jenis-detail" || $_GET['page'] == "jenis" || $_GET['page'] == "detail-page" || $_GET["page"] == "komunitas" || $_GET["page"] == "koleksi-saya") : ?>
+              <a class="nav-link dropdown-toggle" style="color: #0089ed; font-weight: bold;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <?php else : ?>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <?php endif ?>
-              </li>
-              <li>
-                <?php if ($_GET["page"] == "rekomendasi" || $_GET["page"] == "koleksi-saya") : ?>
-                  <a class="dropdown-item active" href="?page=rekomendasi">Adventure</a>
-                <?php else : ?>
-                  <a class="dropdown-item" href="?page=rekomendasi">Adventure</a>
-                <?php endif ?>
-              </li>
-              <li>
-                <?php if ($_GET["page"] == "komunitas") : ?>
-                  <a class="dropdown-item active" href="?page=komunitas">Community</a>
-                <?php else : ?>
-                  <a class="dropdown-item" href="?page=komunitas">Community</a>
-                <?php endif ?>
-              </li>
-            </ul>
+                Program
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <?php if ($_GET["page"] == "detail-page" || $_GET["page"] == "jenis" || $_GET["page"] == "jenis-detail") : ?>
+                      <a class="dropdown-item active" href="?page=jenis">
+                        Biodiversity
+                      </a>
+                    <?php else : ?>
+                      <a class="dropdown-item" href="?page=jenis">
+                        Biodiversity
+                      </a>
+                    <?php endif ?>
+                  </li>
+                  <li>
+                    <?php if ($_GET["page"] == "rekomendasi" || $_GET["page"] == "koleksi-saya") : ?>
+                      <a class="dropdown-item active" href="?page=rekomendasi">Adventure</a>
+                    <?php else : ?>
+                      <a class="dropdown-item" href="?page=rekomendasi">Adventure</a>
+                    <?php endif ?>
+                  </li>
+                  <li>
+                    <?php if ($_GET["page"] == "komunitas") : ?>
+                      <a class="dropdown-item active" href="?page=komunitas">Community</a>
+                    <?php else : ?>
+                      <a class="dropdown-item" href="?page=komunitas">Community</a>
+                    <?php endif ?>
+                  </li>
+                </ul>
           </li>
           <li class="nav-item" style="padding-right: 20px;">
             <?php if ($_GET["page"] == "contact") : ?>
-              <a class="nav-link" style="color: red; font-weight: bold;" href="?page=contact">Contact</a>
+              <a class="nav-link" style="color: #0089ed; font-weight: bold;" href="?page=contact">Contact</a>
             <?php else : ?>
               <a class="nav-link" href="?page=contact">Contact</a>
             <?php endif ?>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <?php if (isset($_SESSION['login'])) : ?>
+            <li class="nav-item">
+              <a href="?page=input-lokasi" class="nav-link" style="padding-right: 30px;">
+                Tambah Titik
+              </a>
+            </li>
+          <?php endif ?>
+          <li class="nav-item" style="padding-right: 20px;">
+            <a target="_blank" href="maps.php" class="nav-link">
               GIS
             </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" target="_blank" href="maps.php">GIS</a>
-              </li>
-              <?php if (isset($_SESSION['login'])) : ?>
-              <li>
-                <a class="dropdown-item" href="?page=input-lokasi">Tambah Titik</a>
-              </li>
-            <?php endif ?>
-            </ul>
           </li>
           <?php if (empty($_SESSION["id_member"])) : ?>
             <li class="nav-item">
@@ -87,7 +92,7 @@
           <?php if (empty($_SESSION['id_member'])) : ?>
 
           <?php else : ?>
-            <li class="nav-item dropdown" style="padding-right: 20px;">
+            <li class=" nav-item dropdown" style="padding-right: 20px;">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="images/<?= $_SESSION['photo'] ?>" style="border-radius: 50%; overflow: hidden; width: 30px; height: 30px;">
                 <?= $_SESSION["username"] ?>
@@ -105,4 +110,4 @@
         </ul>
       </div>
     </div>
-  </nav>
+</nav>
